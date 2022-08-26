d4595ecf2bc3f5738d972f3fc452b7ebe4d586d8
BorisSTL <stalevsky.boris@yandex.ru>
2022-08-26 15:19:07 +0300

    add favicon

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+//let cont = document.querySelector('.content');
+//cont.insertAdjacentHTML("beforeend", `<div class= "divpClass"><a href = #>up to page</a></div>`);
+//let divp = document.createElement('div');
+
+//divp.innerHTML = '<a href = #>up to page</a>';
+//divp.className = 'divpClass';
+
+//cont.append(divp);
+
+//let divp2 = divp.cloneNode(true);
+//divp2.querySelector('a').innerHTML = 'Hi';
+//divp.after(divp2);
+
+//setTimeout(function () {
+//	divp.remove()}, 2000);
